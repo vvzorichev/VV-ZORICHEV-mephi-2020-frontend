619ac90e607c2db9b0f312db1862c4a33e6ed8f8
Vitaly <vitalyzorich@gmail.com>
2020-07-06 19:38:42 +0300

    fix nav

--- src/app/my-cloud-page/main-block/component/main-block.jsx
+++ src/app/my-cloud-page/main-block/component/main-block.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Toolbar from '../toolbar/component';
 import FileList from '../file-list/component';
 
-const MainBlock = ({ section }) => {
+const MainBlock = () => {
 	return (
 		<main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
 			<Toolbar />					
-			<FileList section={section} />
+			<FileList />
 		</main>
 	);
 };
